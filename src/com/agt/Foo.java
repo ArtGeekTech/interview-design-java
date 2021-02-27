ff815522a8d9988a23b733e193bfb3c2f438843e
ArtGeekTech <agt@ArtGeekTechs-Mac-mini.local>
2021-02-26 23:31:54 -0800

    init

---
+++ src/com/agt/Foo.java
@@ -0,0 +1,10 @@
+package com.agt;
+
+public class Foo {
+    static String name = "foo";
+}
+
+class Bar {
+    static String name = "bar";
+
+}
---
+++ src/com/agt/subpkg/Bar.java
@@ -0,0 +1,9 @@
+package com.agt.subpkg;
+
+class Foo {
+    static String name = "foo";
+}
+
+public class Bar {
+    public static String name = "bar";
+}
